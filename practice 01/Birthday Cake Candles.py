c01afba64ac93550e0e4c5f3d7b2dbb66315ba4f
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-08-03 10:33:30 +0530

    Update Birthday Cake Candles.py

--- practice 01/Birthday Cake Candles.py
+++ practice 01/Birthday Cake Candles.py
@@ -14,7 +14,13 @@ import sys
 #
 
 def birthdayCakeCandles(candles):
-    # Write your code here
+    # Find the maximum height of the candles
+    max_height = max(candles)
+    
+    # Count how many candles have the maximum height
+    count_max_height = candles.count(max_height)
+    
+    return count_max_height
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
